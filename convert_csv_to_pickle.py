297678bbe071b07e10deae228a3ad8a2df8ad400
Wang LYU <wlyu2@uwo.ca>
2019-03-07 21:23:20 -0500

    rename this script

---
+++ convert_csv_to_pickle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import os
+import pickle
+
+pre_pickle_dict = {}
+docs_dict = {}
+counter = 0
+doc_id = ''
+hadoop_dir = '/home/cloudera/'
+hadoop_output = 'index.csv'
+file = open(hadoop_dir+hadoop_output,'r')
+for line in file.readlines():
+    line = line.strip()
+    fields = line.split('\t')
+    token = fields[0]
+    doc_name = fields[1]
+    occur_times = fields[2]
+    positions = fields[3:]
+    if not doc_name in docs_dict.values():
+        counter = counter + 1
+        doc_id = 'D'+str(counter)
+        docs_dict[doc_id] = doc_name
+    else:
+        for id,name in docs_dict.items():
+            if name == doc_name:
+                doc_id = id
+    
+    if not token in pre_pickle_dict.keys():
+        pre_pickle_dict[token] = {(doc_id,occur_times):positions}
+    else:
+        pre_pickle_dict[token].update({(doc_id,occur_times):positions})
+file.close()
+
+with open(hadoop_dir+'doc_dict','w') as file:
+    pickle.dump(docs_dict,file)
+
+with open(hadoop_dir+'index','w') as file:
+    pickle.dump(pre_pickle_dict,file)
